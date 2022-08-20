6b132127ffd7fa6fbabe7a155452289d68dbedf5
niloy143 <niloymahmudapu@gmail.com>
2022-08-20 12:40:36 +0600

    sherlock and the valid strings

--- sherlock-and-the-valid-string.js
+++ sherlock-and-the-valid-string.js
@@ -1,26 +1,59 @@
+// Check if the string has the equal number of character or not. also you can remove 1 character to fulfill the condition
+
 function isValid(theString) {
     const stringToArray = theString.split('');
     const uniqueArray = [];
     let multipleArray = [];
-    let countArray = [];
     for (const indexes of stringToArray) {
-        if(uniqueArray.includes(indexes)) {
+        if (uniqueArray.includes(indexes)) {
             continue;
         }
         uniqueArray.push(indexes);
     }
-    for(let i=0; i<uniqueArray.length; i++){
+    for (let i = 0; i < uniqueArray.length; i++) {
         multipleArray[i] = [];
-        for(let j=0; j<stringToArray.length; j++) {
-            if(uniqueArray[i] === stringToArray[j]){
+        for (let j = 0; j < stringToArray.length; j++) {
+            if (uniqueArray[i] === stringToArray[j]) {
                 multipleArray[i].push(stringToArray[j]);
             }
         }
     }
-    for(let i=0; i<multipleArray.length; i++) {
+
+    let countArray = [];
+    for (let i = 0; i < multipleArray.length; i++) {
         countArray.push(multipleArray[i].length);
     }
-    return countArray;
+    let uniqueCountArray = [];
+    let multipleCountArray = [];
+    for (const array of countArray) {
+        if (uniqueCountArray.includes(array)) {
+            continue;
+        }
+        uniqueCountArray.push(array);
+    }
+    for (let i = 0; i < uniqueCountArray.length; i++) {
+        multipleCountArray[i] = [];
+        for (let j = 0; j < countArray.length; j++) {
+            if (uniqueCountArray[i] === countArray[j]) {
+                multipleCountArray[i].push(countArray[j]);
+            }
+        }
+    }
+
+    if(multipleCountArray.length === 1) {
+        return 'YES';
+    }
+    else if (multipleCountArray.length === 2 && (multipleCountArray[0].length === 1 || multipleCountArray[1].length === 1)) {
+        if(multipleCountArray[0][0]+1 === multipleCountArray[1][0] || multipleCountArray[0][0]-1 === multipleCountArray[1][0]) {
+            return 'YES';
+        }
+    }
+    return 'NO';
 }
 
-console.log(isValid('abcc'));
+console.log(isValid("aabbccddeefghi"))
+console.log(isValid("aabbcd"))
+console.log(isValid("abcdefghhgfedecba"))
+console.log(isValid("abc"))
+console.log(isValid("abcc"))
+console.log(isValid("abccc"))
